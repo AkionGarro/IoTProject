20d31062450f7c9aa01371c74929e220f8ae0077
AkionGarro <carloscamp1008@estudianTEC.cr>
2022-11-24 19:00:52 -0600

    Update create devices and list all

--- frontend/angular/app/src/app/components/consumption/consumption.component.ts
+++ frontend/angular/app/src/app/components/consumption/consumption.component.ts
@@ -1,31 +1,58 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { MatCardModule } from '@angular/material/card';
+import { Router } from '@angular/router';
+import { FirestoreService } from 'src/app/services/firestore.service';
+import Swal from 'sweetalert2';
 @Component({
   selector: 'app-consumption',
   templateUrl: './consumption.component.html',
   styleUrls: ['./consumption.component.css'],
 })
 export class ConsumptionComponent implements OnInit {
-  devices: any[] = [
-    {
-      name: 'Fridge',
-      category: 'Energy',
-    },
-    {
-      name: 'Washing machine',
-      category: 'Water',
-    },
-    {
-      name: 'TV',
-      category: 'Energy',
-    },
-  ];
-  constructor() {}
+  formDevice!: FormGroup;
+  submitted = false;
+  devices: any;
 
-  ngOnInit(): void {}
+  constructor(
+    private api: FirestoreService,
+    private formBuilder: FormBuilder,
+    private router: Router
+  ) {}
 
-  addDevice() {}
+  ngOnInit(): void {
+    this.formDevice = this.formBuilder.group({
+      name: ['', Validators.required],
+      category: ['', Validators.required],
+    });
+    this.getDevices();
+  }
 
   detailsDevice() {}
+
+  onSubmit() {
+    this.submitted = true;
+    if (this.formDevice.invalid) {
+      Swal.fire('Petición Incompleta', 'Verifique los campos', 'error');
+      return;
+    } else {
+      var formData: any = new FormData();
+      formData.append('name', this.formDevice.value.name);
+      formData.append('category', this.formDevice.value.category);
+      formData.append('user', localStorage.getItem('localUser'));
+      this.api.addDevice(formData).subscribe((data) => {
+        Swal.fire('Petición Completada', 'Dispositivo agregado', 'success');
+        this.router.navigate(['/consumption']);
+      });
+    }
+  }
+
+  getDevices() {
+    var formData: any = new FormData();
+    formData.append('user', localStorage.getItem('localUser'));
+    this.api.getUserDevices(formData).subscribe((data) => {
+      this.devices = data;
+    });
+  }
 }
